4dbea6fb889e23699fedaff936fc38e1f38ae9b3
michael pm <michaelpmaurerii@gmail.com>
2023-04-01 13:08:24 -0400

    added dummy login screen

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-const gameCard = document.getElementById('game-card')
-const windowResY = window.screen.height
+const gameCard = document.getElementById('game-card');
+const userForm = document.getElementById('user-form');
+const loginCard = document.getElementById('login-card')
+const windowResY = window.screen.height;
 //bases card size off of user's screen resolution
 function init(){
-    gameCard.style.height = (windowResY*0.8)+"px"
-    gameCard.style['margin-top'] = '1.5%'
+    gameCard.style.display = "none";
+    gameCard.style.height = (windowResY*0.8)+"px";
+    gameCard.style['margin-top'] = '1.5%';
+    loginCard.style.height = (windowResY*0.3)+"px";
+    userForm.addEventListener('submit',(e) => {
+        e.preventDefault()
+        gameCard.style.display ="block";
+        loginCard.style.display = "none";
+
+    })
+
 }
 
 init()
