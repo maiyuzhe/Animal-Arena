46e8bb4d1b0faf3f8db2458ed9109f534d6c96b2
maibijiben <michaelpmaurerii@gmail.com>
2023-04-03 16:35:15 -0400

    added random card system

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ const windowResY = window.screen.height;
 //yes and no buttons
 const yes = document.getElementById('yes')
 const no = document.getElementById('no')
-let usedAnimal = []
+let randomArray = []
+generateArray()
+//can toggle devmode by commenting it out
+devMode()
 
 //Survival Score and Username
 let survivalScore = 0
@@ -20,7 +23,7 @@ function init(){
     gameCard.style['margin-top'] = '1.5%';
     loginCard.style.height = (windowResY*0.4)+"px";
     //puts login fields in middle of div
-    for(let i = 0; i < (windowResY/200); i++){
+    for(let i = 0; i < (windowResY/800); i++){
         loginCard.prepend(document.createElement('br'))
     }
     //hides form and makes game card appear
@@ -32,33 +35,24 @@ function init(){
             gameCard.style.display ="block";
             loginCard.style.display = "none";
             gameResults.style.display = "none"
-            renderRandomCreature()
+            renderRandomCreature(randomArray[0])
         }
     })
 
 }
 //grabs random creature from api
-function renderRandomCreature() {
-    let id = Math.floor(Math.random() * 20) + 1;
-    const filteredAnimal = usedAnimal.find((elem) => {
-        while (elem == id){
-            const arrayCheck = [];
-            arrayCheck.push(elem)
-            console.log(arrayCheck)
-            return console.log("on to the next item in the list")
-        }
-        fetch(`http://localhost:3000/creatures/${id}`)
-        .then(response => response.json())
-        .then(data => {
-            creatureImg.src = data.image;
-            creatureImg.alt = data.name
-            document.querySelector('h2').innerText = data.name
-            gameResults.style.display = 'none'
-        })
+function renderRandomCreature(id) {
+    console.log(randomArray)
+    fetch(`http://localhost:3000/creatures/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        creatureImg.src = data.image;
+        creatureImg.alt = data.name
+        document.querySelector('h2').innerText = data.name
+        gameResults.style.display = 'none'
     })
-    usedAnimal = [id, ...usedAnimal]
-    
-
+    //removes the first index of the array each time this function is called
+    randomArray.shift()
 }
 
 yes.addEventListener('click', () => {
@@ -68,7 +62,7 @@ yes.addEventListener('click', () => {
     setTimeout(() => { 
         yes.style.display = 'inline-block'
         no.style.display = 'inline-block'
-        renderRandomCreature()}, 500);
+        renderRandomCreature(randomArray[0])}, 500);
         //change delay back to 3000 later
 })
 
@@ -79,7 +73,7 @@ no.addEventListener('click', () => {
     setTimeout(() => { 
         yes.style.display = 'inline-block'
         no.style.display = 'inline-block'
-        renderRandomCreature()}, 500);
+        renderRandomCreature(randomArray[0])}, 500);
         //change delay back to 3000 later
 })
 
@@ -95,6 +89,20 @@ function devMode(){
         gameResults.style.display = "none"
         survivalScore = 10;
         username = "dev";
-        renderRandomCreature()
+        renderRandomCreature(randomArray[0])
     })
+}
+
+//populates random array with non-repeating integers
+function generateArray(){
+    for (let i = 0; i < 20; i++){
+        let id = Math.floor(Math.random() * 20) + 1;
+        if(randomArray.includes(id) == true){
+            i=i-1;
+        }else{
+            if(id>20==false){
+                randomArray.push(id);
+            }
+        }
+    }
 }
